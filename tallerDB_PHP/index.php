d1a7ef116335aade7bf23c1f2469816cae96ab6f
jdcarvajalc <jdcc1308@gmail.com>
2024-05-15 15:44:23 -0500

    delete CSS file for CRUD pages and update index.php with a new login form
    The `estilosCRUD.css` file was deleted and its contents are no longer used in the project. The `index.php` file

--- tallerDB_PHP/index.php
+++ tallerDB_PHP/index.php
@@ -4,11 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/estilosIndex.css">
+    <title>Iniciar sesión</title>
 </head>
 
 <body>
-
+    <div class="contenedorLogin">
+        <form action="login.php" method="post">
+            <h1>Iniciar sesión</h1>
+            <div class="contenedorInputs">
+                <div class="contenedorInputUsuario">
+                    <label for="user">Usuario</label>
+                    <input class="inputs" type="text" name="user" id="user" placeholder="Escriba su nombre de usuario" required>
+                </div>
+                <div class="contenedorInputContrasena">
+                    <label for="password">Constraseña</label>
+                    <input class="inputs" type="password" name="password" id="password" placeholder="Escriba su contraseña" required>
+                </div>
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
 </body>
 
 </html>
