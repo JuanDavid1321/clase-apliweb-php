da9982fcb53db320aa0e6cdebb80f0bd05600b76
jdcarvajalc <jdcc1308@gmail.com>
2024-05-15 12:57:31 -0500

    updated index.php with examples of PHP concepts: cookies, variables, strings, numbers, type casting, math functions, constants, and operators. Added links to external resources.

--- ejemplo1/index.php
+++ ejemplo1/index.php
@@ -8,26 +8,25 @@
 </head>
 
 <body>
-    <?php 
-        $cookieNombre = "nombre";
-        $cookieValor = "Juan David";
-        setcookie($cookieNombre, $cookieValor, time()+(86400*30), "/"); // Establecer una cookie con nombre y valor, guardada durante 1 mes
+    <?php
+    $cookieNombre = "nombre";
+    $cookieValor = "Juan David";
+    setcookie($cookieNombre, $cookieValor, time() + (86400 * 30), "/"); // Establecer una cookie con nombre y valor, guardada durante 1 mes
 
-        //Verificar si la cookie existe
-        if(!isset($_COOKIE[$cookieNombre])){
-            echo "Cookie no existente." . "<br>";
-        }
-        else{
-            echo "Cookie existente" . "<br>";
-            echo "Nombre: " . $_COOKIE[$cookieNombre];
-        }
+    //Verificar si la cookie existe
+    if (!isset($_COOKIE[$cookieNombre])) {
+        echo "Cookie no existente." . "<br>";
+    } else {
+        echo "Cookie existente" . "<br>";
+        echo "Nombre: " . $_COOKIE[$cookieNombre];
+    }
     ?>
-    <header><?php include 'menu.php';?></header>
+    <header><?php include 'menu.php'; ?></header>
     <h2>Esto es HTML puro</h2>
     <?php
-        $texto = "Mi primer script en php";
-        echo "Texto $texto</br>";
-        print "<h3>Otro texto</h3>"
+    $texto = "Mi primer script en php";
+    echo "Texto $texto</br>";
+    print "<h3>Otro texto</h3>"
     ?>
 
     <p>Este texto está fuera de PHP</p>
@@ -36,267 +35,267 @@
 
     <p>
         <?=
-            // Concatenar una cadena de texto
-            "Hola " . "¿Cómo estás?"
+        // Concatenar una cadena de texto
+        "Hola " . "¿Cómo estás?"
 
-            /*
+        /*
              * Comentario de múltiples líneas
              */
-            ?>
+        ?>
     </p>
 
     <h3>Ejemplos de variables en PHP</h3>
     <?php
-        $x = 15;
-        $y = 20;
-        $mult = $x * $y;
+    $x = 15;
+    $y = 20;
+    $mult = $x * $y;
     ?>
     <?= "Multiplicación de x, y = $mult, y la suma es" . $x + $y . "</br>"; ?>
 
     <?php
-        // Para conocer el tipo de dato se utiliza var_dump()
-        echo "El tipo de dato de x es: ";
-        echo var_dump($x);
-        echo "</br>";
-        echo "El tipo de dato de y es: ";
-        echo var_dump($y);
-        echo "</br>";
+    // Para conocer el tipo de dato se utiliza var_dump()
+    echo "El tipo de dato de x es: ";
+    echo var_dump($x);
+    echo "</br>";
+    echo "El tipo de dato de y es: ";
+    echo var_dump($y);
+    echo "</br>";
     ?>
 
     <h3>Ejemplo de variables globales</h3>
     <?php
-        $saludo = "Hola mundo";
+    $saludo = "Hola mundo";
 
-        //Probando sin utilizar global
-        // function saludar(){
-        //     echo $saludo;
-        // }
-        // saludar();
-        
-        //Probando con global
-        function saludar2()
-        {
-            global $saludo;
-            echo $saludo . "</br>";
-        }
-        saludar2();
+    //Probando sin utilizar global
+    // function saludar(){
+    //     echo $saludo;
+    // }
+    // saludar();
 
-        //Probando con $GLOBALS[]
-        $a = 15;
-        $b = 10;
-        function imprimirGlobales()
-        {
-            echo $GLOBALS["a"] + $GLOBALS["b"] . "</br>";
-        }
-        imprimirGlobales();
+    //Probando con global
+    function saludar2()
+    {
+        global $saludo;
+        echo $saludo . "</br>";
+    }
+    saludar2();
+
+    //Probando con $GLOBALS[]
+    $a = 15;
+    $b = 10;
+    function imprimirGlobales()
+    {
+        echo $GLOBALS["a"] + $GLOBALS["b"] . "</br>";
+    }
+    imprimirGlobales();
     ?>
 
     <h3>Sección de tipo de datos</h3>
     <?php
-        // === Tipos de datos ===
-        
-        // String
-        $x = "Hello world!";
-        $y = 'Hello world!';
+    // === Tipos de datos ===
 
-        var_dump($x);
-        echo "<br>";
-        var_dump($y);
+    // String
+    $x = "Hello world!";
+    $y = 'Hello world!';
 
-        // Int
-        $x = 10.365;
-        var_dump($x);
-        echo "<br>";
+    var_dump($x);
+    echo "<br>";
+    var_dump($y);
 
-        // Float
-        $x = 10.365;
-        var_dump($x);
-        echo "<br>";
+    // Int
+    $x = 10.365;
+    var_dump($x);
+    echo "<br>";
 
-        // Boolean
-        $x = true;
-        var_dump($x);
-        echo "<br>";
+    // Float
+    $x = 10.365;
+    var_dump($x);
+    echo "<br>";
 
-        // Array
-        $cars = array("Volvo", "BMW", "Toyota");
-        var_dump($cars);
-        echo "<br>";
+    // Boolean
+    $x = true;
+    var_dump($x);
+    echo "<br>";
 
-        // Object
-        class Car
+    // Array
+    $cars = array("Volvo", "BMW", "Toyota");
+    var_dump($cars);
+    echo "<br>";
+
+    // Object
+    class Car
+    {
+        public $color;
+        public $model;
+        public function __construct($color, $model)
         {
-            public $color;
-            public $model;
-            public function __construct($color, $model)
-            {
-                $this->color = $color;
-                $this->model = $model;
-            }
-            public function message()
-            {
-                return "My car is a " . $this->color . " " . $this->model . "!";
-            }
+            $this->color = $color;
+            $this->model = $model;
         }
+        public function message()
+        {
+            return "My car is a " . $this->color . " " . $this->model . "!";
+        }
+    }
 
-        $myCar = new Car("red", "Volvo");
-        var_dump($myCar);
-        echo "<br>";
+    $myCar = new Car("red", "Volvo");
+    var_dump($myCar);
+    echo "<br>";
 
-        // Null
-        $x = null;
-        var_dump($x);
-        echo "<br>";
+    // Null
+    $x = null;
+    var_dump($x);
+    echo "<br>";
 
-        // Cambiar el tipo de dato
-        $x = 5;
-        $x = (string) $x;
-        var_dump($x);
+    // Cambiar el tipo de dato
+    $x = 5;
+    $x = (string) $x;
+    var_dump($x);
     ?>
 
     <h3>Sección de Strings</h3>
     <?php
-        // Longitud de String
-        echo strlen("Hello workd!");
-        echo "</br>";
+    // Longitud de String
+    echo strlen("Hello workd!");
+    echo "</br>";
 
-        // Contador de palabras
-        echo str_word_count("Hello world!");
-        echo "</br>";
+    // Contador de palabras
+    echo str_word_count("Hello world!");
+    echo "</br>";
 
-        // Buscar texto dentro de una cadena
-        $texto = "Hola como estas";
-        echo strpos($texto, "estas");
-        echo "</br>";
+    // Buscar texto dentro de una cadena
+    $texto = "Hola como estas";
+    echo strpos($texto, "estas");
+    echo "</br>";
 
-        // Convertir a mayúsculas
-        echo strtoupper($texto);
-        echo "</br>";
+    // Convertir a mayúsculas
+    echo strtoupper($texto);
+    echo "</br>";
 
-        // Convertir a minúsculas
-        echo strtolower($texto);
-        echo "</br>";
+    // Convertir a minúsculas
+    echo strtolower($texto);
+    echo "</br>";
 
-        // Reemplazar un String
-        $x = "Hello world!";
-        echo str_replace("world", "PHP", $x);
-        echo "</br>";
+    // Reemplazar un String
+    $x = "Hello world!";
+    echo str_replace("world", "PHP", $x);
+    echo "</br>";
 
-        // Invertir un String
-        echo strrev($x);
-        echo "</br>";
+    // Invertir un String
+    echo strrev($x);
+    echo "</br>";
 
-        // Eliminar espacios en blanco en el String
-        $y = "   Hola     ";
-        echo trim($y);
-        echo "</br>";
+    // Eliminar espacios en blanco en el String
+    $y = "   Hola     ";
+    echo trim($y);
+    echo "</br>";
 
-        // Convertir String a Array
-        $x = "Hello World!";
-        $y = explode(" ", $x);
-        print_r($y);
-        echo "</br>";
+    // Convertir String a Array
+    $x = "Hello World!";
+    $y = explode(" ", $x);
+    print_r($y);
+    echo "</br>";
     ?>
-    
+
     <h3>Sección Números</h3>
     <?php
-        // Validar si el tipo de dato es entero
-        $x = 5985;
-        var_dump(is_int($x));
-        echo "</br>";
-        $x = 59.85;
-        var_dump(is_int($x));
-        echo "</br>";
+    // Validar si el tipo de dato es entero
+    $x = 5985;
+    var_dump(is_int($x));
+    echo "</br>";
+    $x = 59.85;
+    var_dump(is_int($x));
+    echo "</br>";
 
-        // Validar si el tipo de dato es flotante
-        $x = 10.365;
-        var_dump(is_float($x));
-        echo "</br>";   
+    // Validar si el tipo de dato es flotante
+    $x = 10.365;
+    var_dump(is_float($x));
+    echo "</br>";
     ?>
 
     <h3>Sección de transformaciones de tipo de datos</h3>
     <?php
-        // float a int
-        $x = 23465.768;
-        $int_cast = (int)$x;
-        echo $int_cast;
-        echo "</br>";
+    // float a int
+    $x = 23465.768;
+    $int_cast = (int)$x;
+    echo $int_cast;
+    echo "</br>";
 
-        // string a int
-        $x = "23465.768";
-        $int_cast = (int)$x;
-        echo $int_cast;
-        echo "</br>";
+    // string a int
+    $x = "23465.768";
+    $int_cast = (int)$x;
+    echo $int_cast;
+    echo "</br>";
 
-        // Varias transformaciones
-        $a = 5;       // Integer
-        $b = 5.34;    // Float
-        $c = "hello"; // String
-        $d = true;    // Boolean
-        $e = NULL;    // NULL
+    // Varias transformaciones
+    $a = 5;       // Integer
+    $b = 5.34;    // Float
+    $c = "hello"; // String
+    $d = true;    // Boolean
+    $e = NULL;    // NULL
 
-        $a = (string) $a;
-        $b = (string) $b;
-        $c = (string) $c;
-        $d = (string) $d;
-        $e = (string) $e;
+    $a = (string) $a;
+    $b = (string) $b;
+    $c = (string) $c;
+    $d = (string) $d;
+    $e = (string) $e;
 
-        //To verify the type of any object in PHP, use the var_dump() function:
-        var_dump($a);
-        echo "</br>";
-        var_dump($b);
-        echo "</br>";
-        var_dump($c);
-        echo "</br>";
-        var_dump($d);
-        echo "</br>";
-        var_dump($e);
-        echo "</br>";
+    //To verify the type of any object in PHP, use the var_dump() function:
+    var_dump($a);
+    echo "</br>";
+    var_dump($b);
+    echo "</br>";
+    var_dump($c);
+    echo "</br>";
+    var_dump($d);
+    echo "</br>";
+    var_dump($e);
+    echo "</br>";
     ?>
 
     <h3>Sección Matemáticas</h3>
     <?php
-        //Función pi
-        echo(pi());
-        echo "</br>";
+    //Función pi
+    echo (pi());
+    echo "</br>";
 
-        // Funciones de mínimo y máximo
-        echo(min(0, 150, 30, 20, -8, -200));
-        echo "</br>";
-        echo(max(0, 150, 30, 20, -8, -200));
-        echo "</br>";
+    // Funciones de mínimo y máximo
+    echo (min(0, 150, 30, 20, -8, -200));
+    echo "</br>";
+    echo (max(0, 150, 30, 20, -8, -200));
+    echo "</br>";
 
-        // Función absoluto
-        echo(abs(-76));
-        echo "</br>";
+    // Función absoluto
+    echo (abs(-76));
+    echo "</br>";
 
-        // Función raíz cuadrada
-        echo(sqrt(49));
-        echo "</br>";
+    // Función raíz cuadrada
+    echo (sqrt(49));
+    echo "</br>";
 
-        // Función redondeo
-        echo(round(12.345));
-        echo "</br>";
+    // Función redondeo
+    echo (round(12.345));
+    echo "</br>";
 
-        // Números aleatorios
-        $numeroAleatorio = rand();
-        echo("Número Aleatorio: " .$numeroAleatorio);
-        echo "</br>";
+    // Números aleatorios
+    $numeroAleatorio = rand();
+    echo ("Número Aleatorio: " . $numeroAleatorio);
+    echo "</br>";
     ?>
 
     <h3>Sección constantes</h3>
     <?php
-        // Crear una constate
-        define('PI', 3.1416);
-        echo PI;
-        echo "</br>";
+    // Crear una constate
+    define('PI', 3.1416);
+    echo PI;
+    echo "</br>";
 
-        // Crear una constante con la palabra clave "const"
-        const EULER = 2.71828;
-        echo EULER;
-        echo "</br>";
+    // Crear una constante con la palabra clave "const"
+    const EULER = 2.71828;
+    echo EULER;
+    echo "</br>";
 
-        /*
+    /*
             PHP tiene varias constantes predefinidas que se pueden utilizar en el código. Estas constantes son:
             __CLASS__	        If used inside a class, the class name is returned.	
             __DIR__	            The directory of the file.	
@@ -313,8 +312,8 @@
 
     <h3>Sección operadores</h3>
     <?php
-        echo "<h4>Operadores aritméticos</h4>";
-        echo '<table>
+    echo "<h4>Operadores aritméticos</h4>";
+    echo '<table>
             <tr>
                 <th>Operation</th>
                 <th>Symbol</th>
@@ -358,10 +357,11 @@
                 <td>Result of raising $x to the $y\'th power</td>
             </tr>
         </table>';
-        echo "</br>";
-        echo "para más operadores, visite: ";
-        echo '<a href="https://www.w3schools.com/php/php_operators.asp">LINK</a>'; 
-        
+    echo "</br>";
+    echo "para más operadores, visite: ";
+    echo '<a href="https://www.w3schools.com/php/php_operators.asp">LINK</a>';
+
     ?>
 </body>
+
 </html>
---
+++ tallerDB_PHP/consultas.sql
@@ -0,0 +1,26 @@
+-- Consulta general
+SELECT 
+    pokemon.pok_id, 
+    pokemon.pok_name, 
+    pokemon.pok_height, 
+    pokemon.pok_weight, 
+    pokemon.pok_base_experience,
+    base_stats.b_hp,
+    base_stats.b_atk,
+    base_stats.b_def,
+    pokemon_types.slot,
+    types.type_name
+FROM pokemon
+JOIN base_stats 
+ON base_stats.pok_id = pokemon.pok_id 
+JOIN pokemon_types 
+ON pokemon.pok_id = pokemon_types.pok_id 
+JOIN types 
+ON types.type_id = pokemon_types.type_id
+ORDER BY pokemon.pok_id;
+
+DELETE p, pt, bs
+FROM pokemon p
+INNER JOIN pokemon_types pt ON p.pok_id = pt.pok_id
+INNER JOIN base_stats bs ON p.pok_id = bs.pok_id
+WHERE p.pok_id > 40;
